d0acea8657a40c54a367aef2f26cf063e9f05780
Abdalmoamen Abbara <abomajedabbara@gmail.com>
2025-03-11 22:33:28 +0300

    added tabLogo

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,13 @@
 import "./globals.css"
 import "@/public/style.css"
+import tabLogo from "@/public/tabLogo.png"
 
 export const metadata = {
     title: "Abdalmoamen Abbara",
     description: "Abdalmoamen Abbara's Portfolio",
+    icons: {
+        icon: tabLogo.src,
+    },
 }
 
 export default function RootLayout({ children }) {
